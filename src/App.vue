21e08436dde09d5f200d3f087511cbf7fc883abe
Thiago Krüger <thiagork@msn.com>
2019-08-05 22:01:45 +0200

    Added layout/icons, remove button, toggle complete

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,66 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <md-app>
+    <md-app-toolbar class="md-primary">
+      <span class="md-title">Todo App</span>
+    </md-app-toolbar>
+    <md-app-content>
+      <md-list class="todos">
+        <md-list-item>
+          <md-field>
+            <md-input v-model="currentTodo" @keydown.enter="addTodo()" placeholder="Add a todo"></md-input>
+          </md-field>
+        </md-list-item>
+        <md-list-item v-for="todo in todos" @dblclick="editTodo(todo)" :key="todo.id" >
+          <i class="material-icons" @click="toggleTodo(todo)">
+            {{ todo.completed ? "check_box" : "check_box_outline_blank" }}
+          </i>
+            <span :class="{ isComplete: todo.completed}">{{ todo.label }}</span>
+          <i class="material-icons" @click="removeTodo(todo)">
+            close
+          </i>
+        </md-list-item>
+      </md-list>
+    </md-app-content>
+  </md-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      todos: [],
+      currentTodo: ""
+    };
+  },
+  methods: {
+    addTodo() {
+      this.todos.push({
+        id: this.todos.length,
+        label: this.currentTodo,
+        completed: false
+      });
+      this.currentTodo = "";
+    },
+
+    removeTodo(todo) {
+      const index = this.todos.indexOf(todo);
+      this.todos.splice(index, 1);
+    },
+
+    editTodo(todo) {
+      console.log("editing todo");
+    },
+
+    toggleTodo(todo) {
+      const index = this.todos.indexOf(todo);
+      this.todos[index].completed = !this.todos[index].completed;
+    }
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.isComplete {
+  text-decoration: line-through;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,14 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+// import { MdField } from 'vue-material/dist/components';
+import VueMaterial from 'vue-material';
+import 'vue-material/dist/vue-material.min.css';
+import 'vue-material/dist/theme/black-green-light.css'
 
-Vue.config.productionTip = false
+Vue.use(VueMaterial);
+
+Vue.config.productionTip = false;
+Vue.material.locale.dateFormat = 'dd/MM/yyyy';
 
 new Vue({
   render: h => h(App),
